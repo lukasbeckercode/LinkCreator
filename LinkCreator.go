46c05e45967d780a5d17eb0b0bc6ddb163dfae9b
Lukas Becker <lukas.becker.lb@outlook.com>
2020-12-08 23:09:51 +0100

    added support for firefox

--- LinkCreator.go
+++ LinkCreator.go
@@ -26,9 +26,17 @@ func main() {
 
 	linkText := widget.NewLabel("Enter Link: ")      //description Label
 	nameText := widget.NewLabel("Enter Link Name: ") //description Label
+	choice := 0
+	radio := widget.NewRadio([]string{"Microsoft Edge", "Mozilla Firefox"}, func(s string) {
+		if s == "Microsoft Edge" {
+			choice = 0
+		} else {
+			choice = 1
+		}
+	})
 
 	btn := widget.NewButton("Create", func() { //Button press function
-		write(linkIn.Text, nameIn.Text) //call this function, pass the values of the text-boxes
+		write(linkIn.Text, nameIn.Text, choice) //call this function, pass the values of the text-boxes
 		//reset text-boxes
 		linkIn.SetText("")
 		nameIn.SetText("")
@@ -39,7 +47,7 @@ func main() {
 	hor2 := widget.NewHBox(nameText, nameIn) //Line for link name
 
 	//This container spaces the boxes and the button
-	container := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), hor2, hor1, layout.NewSpacer(), btn)
+	container := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), hor2, hor1, layout.NewSpacer(), radio, layout.NewSpacer(), btn)
 	w.SetContent(container)
 
 	w.Resize(fyne.NewSize(500, 150)) //fixed size
@@ -47,13 +55,18 @@ func main() {
 
 }
 
-func write(linkIn string, nameIn string) {
+func write(linkIn string, nameIn string, browser int) {
 	//This function creates a new batch file and adds the command to it
 	filename := nameIn + ".bat"      //create a valid file name
 	file, err := os.Create(filename) //create the batch file
+	var content string
+	if browser == 0 {
+		content = "start Microsoft-Edge:\"" + linkIn + "\"" //create the command
+	} else {
+		content = "start firefox " + linkIn
+	}
 
-	content := "start Microsoft-Edge:\"" + linkIn + "\"" //create the command
-	writer := bufio.NewWriter(file)                      //make a writer
+	writer := bufio.NewWriter(file) //make a writer
 	if err != nil {
 		log.Fatal(err) //Error handling
 	}
